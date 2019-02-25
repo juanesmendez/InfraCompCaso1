4f84d06601afaa442f1087edf2141514cced3abf
Juan Esteban Méndez Roys <je.mendez@uniandes.edu.co>
2019-02-25 16:13:17 -0500

    Lo que sea

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,9 +1,25 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Cliente extends Thread{
+    private int id;
     private int numMensajes;
+    private ArrayList<Mensaje> mensajes;
     private Buffer buffer;
 
+    public Cliente(int id, int numMensajes, Buffer buffer){
+        this.id = id;
+        this.numMensajes = numMensajes;
+        this.buffer = buffer;
+        mensajes = new ArrayList<>();
+    }
 
-
+    @Override
+    public void run() {
+        for(int i = 0; i < numMensajes; i++){
+            int contenido = (int) Math.round(Math.random()*1000);
+            mensajes.add(new Mensaje(id,contenido));
+        }
+    }
 }
--- src/model/Mensaje.java
+++ src/model/Mensaje.java
@@ -2,5 +2,13 @@ package model;
 
 public class Mensaje {
 
+    private int contenido;
+
+    private int idCliente;
+
+    public Mensaje(int idCliente, int contenido){
+        this.idCliente = idCliente;
+        this.contenido = contenido;
+    }
 
 }
